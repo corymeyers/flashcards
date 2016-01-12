fd16e908e8ede5f23ba9e4fb1183b75efa590c0f
Cory Meyers and Simon Temple <whippoorwill.in.flight@gmail.com>
2016-01-11 16:34:11 -0800

    create site using jQuery. This is the flashcard project

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+$(function() {
+  $(".clickable").click(function() {
+    $("#initially-showing").toggle();
+    $("#initially-hidden").toggle();
+  });
+});
+$(function() {
+    $(".clickable1").click(function() {
+      $("#initially-showing1").toggle();
+      $("#initially-hidden1").toggle();
+    });
+});
+$(function() {
+    $(".clickable2").click(function() {
+      $("#initially-showing2").toggle();
+      $("#initially-hidden2").toggle();
+    });
+});
+$(function() {
+    $(".clickable3").click(function() {
+      $("#initially-showing3").toggle();
+      $("#initially-hidden3").toggle();
+    });
+});
+$(function() {
+    $(".clickable4").click(function() {
+      $("#initially-showing4").toggle();
+      $("#initially-hidden4").toggle();
+    });
+});
